0caa27ad2b8fd086274c56221e31e78a74479224
George Repole <grepole121@users.noreply.github.com>
2021-06-14 16:25:31 +0100

    Reformatted testing. Added performance testing images

--- src/test/java/com/sparta/george/MainTest.java
+++ src/test/java/com/sparta/george/MainTest.java
@@ -6,62 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainTest {
-    @Test
-    @DisplayName("Performance test to read file with 10,000 entries")
-    public void performanceTestReadFile1() throws FileNotFoundException {
-        List<Double> times = new ArrayList<>();
-        int numberOfRuns = 1;
-        for (int i = 0; i < numberOfRuns; i++) {
-            double startTime = System.nanoTime();
-
-            ReadFile.readFile(new FileReader("resources/employees.csv"));
-
-            double endTime = System.nanoTime();
-            times.add((endTime - startTime) / 1000000);
-        }
-        double averageTime = times.stream().mapToDouble(Double::doubleValue).sum();
-        System.out.printf("Average time taken to read file after %d runs: %.2fms\n", numberOfRuns, averageTime / numberOfRuns);
-    }
-
-    @Test
-    @DisplayName("Performance test to read file with 10,000 entries")
-    public void performanceTestReadFile10() throws FileNotFoundException {
-        List<Double> times = new ArrayList<>();
-        int numberOfRuns = 10;
-        for (int i = 0; i < numberOfRuns; i++) {
-            double startTime = System.nanoTime();
-
-            ReadFile.readFile(new FileReader("resources/employees.csv"));
-
-            double endTime = System.nanoTime();
-            times.add((endTime - startTime) / 1000000);
-        }
-        double averageTime = times.stream().mapToDouble(Double::doubleValue).sum();
-        System.out.printf("Average time taken to read file after %d runs: %.2fms\n", numberOfRuns, averageTime / numberOfRuns);
-    }
-
-    @Test
-    @DisplayName("Performance test to read file with 10,000 entries")
-    public void performanceTestReadFile100() throws FileNotFoundException {
-        List<Double> times = new ArrayList<>();
-        int numberOfRuns = 100;
-        for (int i = 0; i < numberOfRuns; i++) {
-            double startTime = System.nanoTime();
-
-            ReadFile.readFile(new FileReader("resources/employees.csv"));
-
-            double endTime = System.nanoTime();
-            times.add((endTime - startTime) / 1000000);
-        }
-        double averageTime = times.stream().mapToDouble(Double::doubleValue).sum();
-        System.out.printf("Average time taken to read file after %d runs: %.2fms\n", numberOfRuns, averageTime / numberOfRuns);
-    }
-
     @Test
     @DisplayName("Performance test to add 9943 entries to the database sequentially")
     public void performanceTestAddToDbSeq() throws FileNotFoundException, SQLException {
